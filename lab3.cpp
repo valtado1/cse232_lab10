545260cadd52eb1ea6126e9bd0c86112dc635783
Lydia Valtadoros <valtado1@msu.edu>
2023-03-28 19:20:26 +0000

    updating example cp file

--- lab3.cpp
+++ lab3.cpp
@@ -8,18 +8,18 @@ int main() {
 // WRITE YOUR CODE HERE
   int x;
   while (cin >> x && x > 0) {
-      int ap = 0;
+      int additive_persistence = 0;
       while (x >= 10) {
-        int sum = 0;
+        int curr_sum = 0;
         while (x) {
-          sum += x % 10;
+          curr_sum += x % 10;
           x /= 10;
         }
-      ap ++;
-      x = sum;
+      additive_persistence ++;
+      x = curr_sum;
       }
       //at the end of this loop, end up with additive root
-  cout << ap << ' ' << x << endl;
+  cout << additive_persistence << ' ' << x << endl;
   }
   return 0;
 }
